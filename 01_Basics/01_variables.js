7c2673e8a1a37e5c4d8a42f5a6ee62b9eef5d300
Aviditya <59193874+Aviditya@users.noreply.github.com>
2023-12-09 07:46:26 +0000

    variable&datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const myName = 'Utpal'
+let age = 32;
+var isMarried = false;
+city = 'Pune'
+
+console.log(city);
+//don't use var as it will cause u problem in scope 
+console.table([myName,age,isMarried,city])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" // using newer version oj js
+
+//datatypes
+
+let a = 123
+let b = "utpal"
+let c = 'Mumbai'
+let isMarried = false
+let car = null;
+let energy = undefined;
+
+//null is an object where as others are datatypes (primitive call by ref.)
+ 
