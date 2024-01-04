7746143971d4c0e4d61dcf4e37738845ca34620c
Aviditya <59193874+Aviditya@users.noreply.github.com>
2024-01-04 06:13:54 +0000

    Sum and math

---
+++ Problems/MeterConverter.js
@@ -0,0 +1,11 @@
+// function toMeterNCentimeter(valu){
+//     tometer=valu*1_000;
+//     tocentimeter=valu*1_00_000;
+//     return console.log("The distance given is "+valu+" kilometer. Distance in meter is "+tometer+" and Distance in centimeter is %s",tocentimeter);
+// }
+
+// console.log(toMeterNCentimeter(75.8));
+km=67.9;
+cm=km*1_00_000;
+m=km*1_000;
+console.log(km,cm,m);
