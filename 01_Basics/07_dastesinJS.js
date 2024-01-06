e66856d692ff7fa9cd0ac0a3245e8ca7fc5ca401
Aviditya <59193874+Aviditya@users.noreply.github.com>
2024-01-06 19:24:01 +0000

    date.v1

---
+++ 01_Basics/07_dastesinJS.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toString());
+
+// Practice dates please!
