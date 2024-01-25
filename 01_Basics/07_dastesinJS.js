21f887de0536791041177df3b5f01072e9dec2ac
Aviditya <59193874+Aviditya@users.noreply.github.com>
2024-01-25 15:48:03 +0000

    Array

--- 01_Basics/07_dastesinJS.js
+++ 01_Basics/07_dastesinJS.js
@@ -8,3 +8,14 @@ console.log(myDate.toLocaleDateString());
 console.log(myDate.toString());
 
 // Practice dates please!
+let myTimeStamp = Date.now()// it is in miliseconds
+console.log(myTimeStamp);
+
+let myNewDate = new  Date("22-Jan-2024")
+console.log(myNewDate);
+// A use full date method!
+console.log(
+myDate.toLocaleString('default',{
+    month:"long"
+})
+);
